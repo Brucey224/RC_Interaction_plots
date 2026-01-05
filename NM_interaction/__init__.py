f4c6b4aa5c3e00f5c01af240bc014e941f7fc2ea
james-bruceLSL <james.bruce@londonstructureslab.com>
2026-01-05 08:38:06 +0000

    remove unit test refs that weren't working

--- NM_interaction/__init__.py
+++ NM_interaction/__init__.py
@@ -6,7 +6,6 @@ from .geometryUtils import arrange_circular_bars, arrange_rectangular_bars, comp
 from .plotting import plot_rectangular_section, plot_circular_section
 from .codeChecks import check_moments, check_slenderness, compute_major_axis_slender_moments, compute_minor_axis_slender_moments, compute_creep_coefficients
 from .resistanceCalcs import determine_envelope_value_major_axis_positive, determine_envelope_value_major_axis_negative, determine_envelope_value_minor_axis_positive, determine_envelope_value_minor_axis_negative, compute_plastic_axial_capacity
-from .unitTests import test_positive_rectangular_major_plot, plot_M_contributions, plot_N_contributions, review_steel_strains, test_positive_circular_major_plot
 
 # Define what gets imported when using `from rc_interaction import *`
 __all__ = [
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from NM_interaction import InputApp
-from NM_interaction import review_steel_strains, plot_M_contributions, plot_N_contributions, test_positive_rectangular_major_plot, test_positive_circular_major_plot
 
 if __name__ == "__main__":
     import tkinter as tk
